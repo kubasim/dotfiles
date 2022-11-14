5ee2147327ebd4bf4fa0a10bf96594d25a610f3f
kubasim <77584660+kubasim@users.noreply.github.com>
2022-11-14 11:42:22 +0100

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
 killall -q polybar
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+(sleep 2; polybar bar) &
+#(sleep 2; polybar tray) &
