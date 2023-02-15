d975e84050ac15678fe001f0d161433cc45931f1
kubasim <77584660+kubasim@users.noreply.github.com>
2023-02-15 10:25:32 +0100

    KOMYT

---
+++ polybar/grayblocks/scripts/powermenu.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="~/.config/polybar/grayblocks/scripts/rofi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -no-config -theme $dir/powermenu.rasi"
+
+# Options
+shutdown=" Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend=" Sleep"
+logout=" Logout"
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-no-config\
+        -i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -no-config -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		
+		systemctl poweroff
+	
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		
+		systemctl reboot
+		
+        ;;
+    $lock)
+		if [[ -f /usr/bin/i3lock ]]; then
+			i3lock-fancy
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+		ans=$(confirm_exit &)
+		i3lock-fancy
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
+		
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+
+		if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+			openbox --exit
+		elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+			bspc quit
+		elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+			i3-msg exit
+		fi
+		
+        ;;
+esac
